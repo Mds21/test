1cad95a7230a3e7bf46b32eb8009d2857585ab7f
Mds21 <khansayeed25519@gmail.com>
2022-11-24 12:08:22 +0530

    changes in test

---
+++ a.py
@@ -0,0 +1,20 @@
+# import cv2
+# import webcolors
+
+# image="C:/Users/CheckExplore/Desktop/vi/1211372/left.jpg"
+
+# def bgr_to_hex(bgr):
+#    rgb =list(bgr)
+#    rgb.reverse()
+#    return webcolors.rgb_to_hex(tuple(rgb))
+
+# def FindColors(image):
+#     color_hex = []
+#     for i in image:
+#         for  j in i:
+#             j = list(j)
+#             color_hex.append(bgr_to_hex(tuple(j)))
+#     return set(color_hex)
+
+# color_list = FindColors(image)
+
